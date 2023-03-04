1c3a582dc6de315f14e8725668caeaf9f4c59558
Dissanayake71 <85305564+Dissanayake71@users.noreply.github.com>
2023-03-04 17:14:52 +0530

    add home header component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import HomeHeader from '@/components/HomeHeader';
+
 
 export default function page() {
   return (
-    <div>Home</div>
-  )
+   <>
+   <HomeHeader/>
+   </>
+  );
 }
+
